91ab271266da1e678bb42b136eb8b19be06db613
Martin Lucina <martin@lucina.net>
2019-05-19 19:34:14 +0200

    Rewrite using sd-dbus, all works now

--- Makefile
+++ Makefile
@@ -6,5 +6,5 @@ clean:
 	$(RM) xscreensaver-systemd
 
 CFLAGS += -O2 -Wall -std=c99
-CFLAGS += $(shell pkg-config glib-2.0 dbus-glib-1 --cflags)
-LDLIBS += $(shell pkg-config glib-2.0 dbus-glib-1 --libs)
+CFLAGS += $(shell pkg-config libsystemd --cflags)
+LDLIBS += $(shell pkg-config libsystemd --libs)
--- xscreensaver-systemd.c
+++ xscreensaver-systemd.c
@@ -1,78 +1,144 @@
-#include <glib.h>
-#include <dbus/dbus.h>
-#include <dbus/dbus-glib.h>
-#include <dbus/dbus-glib-lowlevel.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <systemd/sd-bus.h>
+#include <unistd.h>
 
-GMainLoop *loop = NULL;
+struct handler_ctx {
+    sd_bus *bus;
+    int lock_fd;
+};
+static struct handler_ctx global_ctx = { .bus = NULL, .lock_fd = -1 };
 
-void handle_signal_prepare_for_sleep(DBusMessage *msg)
+static int handler(sd_bus_message *m, void *arg,
+        sd_bus_error *ret_error)
 {
-    DBusError error;
-    dbus_error_init(&error);
- 
-    int arg = 0;
- 
-    if (!dbus_message_get_args(msg, &error, DBUS_TYPE_BOOLEAN, &arg,
-            DBUS_TYPE_INVALID)) {
-        g_error("Cannot read PrepareForSleep message, cause: %s", error.message);
-        dbus_error_free(&error);
-        return;
+    struct handler_ctx *ctx = arg;
+    int before_sleep;
+    int rc;
+
+    rc = sd_bus_message_read(m, "b", &before_sleep);
+    if (rc < 0) {
+        fprintf(stderr, "Failed to read message: %s\n", strerror(-rc));
+        return 0;
     }
-    g_message("Signal received. Arg: %d", arg);
-    if (arg) { /* true: Before sleep, false: After sleep */
-        GError *error = NULL;
-        gchar *argv[] = { "/usr/bin/xscreensaver-command", "-lock", NULL };
-        if (!g_spawn_sync(NULL, argv, NULL,
-                G_SPAWN_STDOUT_TO_DEV_NULL | G_SPAWN_STDERR_TO_DEV_NULL,
-                NULL, NULL, NULL, NULL, NULL, &error)) {
-            g_error("Spawn failed: %s", error->message);
-            g_error_free(error);
+    if (before_sleep) {
+        (void)system("/usr/bin/xscreensaver-command -lock");
+
+        if (ctx->lock_fd != -1) {
+            rc = close(ctx->lock_fd);
+            if (rc != 0) {
+                fprintf(stderr, "Failed to release lock: %s\n",
+                        strerror(errno));
+            }
+            ctx->lock_fd = -1;
         }
-        g_main_loop_quit(loop);
     }
+    else {
+        (void)system("xset dpms force on");
+        (void)system("/usr/bin/xscreensaver-command -deactivate");
+
+        sd_bus_error error = SD_BUS_ERROR_NULL;
+        sd_bus_message *m = NULL;
+        rc = sd_bus_call_method(ctx->bus,
+                "org.freedesktop.login1",
+                "/org/freedesktop/login1",
+                "org.freedesktop.login1.Manager",
+                "Inhibit",
+                &error,
+                &m,
+                "ssss",
+                "sleep",
+                "xscreensaver",
+                "lock screen on suspend",
+                "delay");
+        if (rc < 0) {
+            fprintf(stderr, "Failed to call Inhibit(): %s\n", error.message);
+            goto out;
+        }
+        rc = sd_bus_message_read(m, "h", &ctx->lock_fd);
+        if (rc < 0) {
+            fprintf(stderr, "Failed to read message: %s\n", strerror(-rc));
+            goto out;
+        }
+
+out:
+        // XXX drops the lock, why? sd_bus_message_unref(m);
+        sd_bus_error_free(&error);
+    }
+
+    return 0;
 }
 
-DBusHandlerResult signal_filter(DBusConnection *connection, DBusMessage *msg,
-        void *user_data)
+int main(int argc, char *argv[])
 {
-    if (dbus_message_is_signal(msg, "org.freedesktop.login1.Manager",
-            "PrepareForSleep")) {
-        handle_signal_prepare_for_sleep(msg);
+    sd_bus *bus = NULL;
+    sd_bus_slot *slot = NULL;
+    struct handler_ctx *ctx = &global_ctx;
+
+    int rc = sd_bus_open_system(&bus);
+    if (rc < 0) {
+        fprintf(stderr, "Failed to connect to system bus: %s\n", strerror(-rc));
+        goto out;
     }
-    return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
-}
- 
-int main()
-{
-    loop = g_main_loop_new(NULL, FALSE);
-    DBusError error;
- 
-    dbus_error_init(&error);
-    DBusConnection *conn = dbus_bus_get(DBUS_BUS_SYSTEM, &error);
- 
-    if (dbus_error_is_set(&error)) {
-        g_error("Cannot get System BUS connection: %s", error.message);
-        dbus_error_free(&error);
-        return EXIT_FAILURE;
+    ctx->bus = bus;
+
+    sd_bus_error error = SD_BUS_ERROR_NULL;
+    sd_bus_message *m = NULL;
+    rc = sd_bus_call_method(bus,
+            "org.freedesktop.login1",
+            "/org/freedesktop/login1",
+            "org.freedesktop.login1.Manager",
+            "Inhibit",
+            &error,
+            &m,
+            "ssss",
+            "sleep",
+            "xscreensaver",
+            "lock screen on suspend",
+            "delay");
+    if (rc < 0) {
+        fprintf(stderr, "Failed to call Inhibit(): %s\n", error.message);
+        goto out;
     }
-    dbus_connection_setup_with_g_main(conn, NULL);
- 
-    char *rule = "type='signal',interface='org.freedesktop.login1.Manager'";
-    g_message("Signal match rule: %s", rule);
-    dbus_bus_add_match(conn, rule, &error);
- 
-    if (dbus_error_is_set(&error)) {
-        g_error("Cannot add D-BUS match rule, cause: %s", error.message);
-        dbus_error_free(&error);
-        return EXIT_FAILURE;
+    rc = sd_bus_message_read(m, "h", &ctx->lock_fd);
+    if (rc < 0) {
+        fprintf(stderr, "Failed to read message: %s\n", strerror(-rc));
+        goto out;
     }
- 
-    g_message("Listening to D-BUS signals using a connection filter");
-    dbus_connection_add_filter(conn, signal_filter, NULL, NULL);
- 
-    g_main_loop_run(loop);
- 
-    return EXIT_SUCCESS; 
+    // XXX drops the lock, why? sd_bus_message_unref(m);
+
+    const char *match =
+        "type='signal',interface='org.freedesktop.login1.Manager'"
+        ",member='PrepareForSleep'";
+    rc = sd_bus_add_match(bus, &slot, match, handler, &global_ctx);
+    if (rc < 0) {
+        fprintf(stderr, "Failed to add match: %s\n", strerror(-rc));
+        goto out;
+    }
+
+    for (;;) {
+        rc = sd_bus_process(bus, NULL);
+        if (rc < 0) {
+            fprintf(stderr, "Failed to process bus: %s\n", strerror(-rc));
+            goto out;
+        }
+        if (rc > 0)
+            /* we processed a request, try to process another one, right-away */
+            continue;
+
+        /* Wait for the next request to process */
+        rc = sd_bus_wait(bus, (uint64_t) -1);
+        if (rc < 0) {
+            fprintf(stderr, "Failed to wait on bus: %s\n", strerror(-rc));
+            goto out;
+        }
+    }
+
+out:
+    sd_bus_slot_unref(slot);
+    sd_bus_unref(bus);
+    sd_bus_error_free(&error);
+
+    return (rc < 0) ? EXIT_FAILURE : EXIT_SUCCESS;
 }
